88dc00ada05847f8f3831baf780e732337020c82
Andre Ravazzi <amravazzi@gmail.com>
2020-04-07 01:49:35 -0300

    batman

---
+++ src/app.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes/router'
+
+const app = express();
+// const routes = require('./app/api/routes/routes');
+// const routesV1 = require('./app/api/v1/routes/routes');
+
+// Middlewares
+app.use(cors())
+app.use('/v1', routes);
+
+//Catch 404 Erros
+app.use((req, res, next) => {
+    const err = new Error('Not found');
+    err.statusCode = 404;
+    next(err);
+});
+
+//Error handle
+app.use((err, req, res, next) => {
+    const error = app.get('env') === 'development' ? err : {};
+    const status = err.statusCode || 500;
+
+    //respond to client
+    res.status(status).json({
+        error: {
+            message: error.message
+        }
+    });
+
+    //respond to terminal
+    console.error(err);
+});
+
+
+//Start server
+const port = app.get('port') || 8080;
+app.listen(port, () => {console.log(`Server is running on port: ${port}`)});
+
+export default app;
