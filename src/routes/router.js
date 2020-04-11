6f7322e062948a6dfa5a00c519f5093bd8b06438
Andre Ravazzi <amravazzi@gmail.com>
2020-04-11 03:28:37 -0300

    feat(*): update routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import QuoteController from '../controllers/QuoteController';
+import QuoteValidation from '../middlewares/validators/QuoteValidation';
+import CreateQuoteValidation from '../middlewares/validators/CreateQuoteValidation';
 
 const router = Router();
 const quoteController = new QuoteController();
+const quoteValidation = new QuoteValidation();
+const createQuoteValidation = new CreateQuoteValidation();
 
-router.get('/quote/:from/:to', quoteController.quote);
-router.post('/quote', quoteController.create);
+router.get('/quote/:from/:to', quoteValidation.validate, quoteController.quote);
+router.post('/quote', createQuoteValidation.validate, quoteController.create);
 
 export default router;
