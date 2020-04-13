8a9b09d31a0f8bca29a57165c4e2af9cc355c38c
Andre Ravazzi <amravazzi@gmail.com>
2020-04-13 02:34:44 -0300

    feat(*): update controllers

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,18 +1,6 @@
 class Controller {
 	constructor(model) {
 		this._model = model;
-		this.create = this.create.bind(this);
-	}
-
-	create(req, res, next) {
-		const object = new this._model();
-		object.save(req.body);
-
-		res
-		.send({
-			success: true,
-    	message: 'Resource has been saved.'
-		});
 	}
 }
 
--- src/controllers/QuoteController.js
+++ src/controllers/QuoteController.js
@@ -7,17 +7,71 @@ class QuoteController extends Controller {
   }
 
 	async quote(req, res, next) {
-    const q = new Quote();
-    const show = await q.show(5);
+    const { from, to } = req.params;
 
-    let response = { message: `Hello World from Quotes.quote: from ${show[0].departure}` };
+    const quote = new Quote();
+    const departures = await quote.showByDeparture(from);
+
+    // console.log(departures);
+
+    // for(let dep of departures) {
+    //   let { departure, arrival, value } = dep;
+
+    //   let legs = [departure];
+    //   let totalValue = 0;
+
+    //   if(currentArrival === to) {
+    //     legs.push(currentArrival);
+    //     totalValue = value;
+    //   }
+
+      // let currentDepature = departure;
+      // let currentArrival = arrival;
+
+      // let currentDepature = arrival;
+
+
+      // let nextArrivals = await quote2.showByDeparture(currentDepature);
+
+      // for(let nxt of nextArrivals) {
+
+      // }
+
+      // console.log(possibleArrivals);
+    // }
+
+    let response = { message: `Hello World from Quotes.quote: from ${departures[0].from}` };
     res.status(200).json(response);
 	}
 
-	create(req, res) {
-		console.log(req.body);
-    let response = { message: `Hello World from Quotes.create: ${req.body.from}` };
-    res.status(200).json(response);
+  iterateRoutes(routes) {
+    const quote = new Quote();
+    let nextArrivals = await quote2.showByDeparture(currentDepature);
+  }
+
+	async create(req, res) {
+		const quote = new Quote();
+    const { from, to, price } = req.body;
+
+    let message = {};
+    let status = 200;
+
+    try {
+      await quote.create({ from, to, price });
+      message = {
+        success: true,
+        message: 'Resource has been saved.'
+      };
+    } catch(e) {
+      status = 500;
+      message = {
+        success: false,
+        message: 'Resource could not be saved.',
+        error: e
+      };
+    }
+
+    res.status(status).send(message);
 	}
 }
 
