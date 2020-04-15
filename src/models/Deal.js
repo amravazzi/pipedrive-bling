6a09b23aa4734353536bea9bea256ab9bfe9f34b
Andre Ravazzi <amravazzi@gmail.com>
2020-04-15 04:36:32 -0300

    feat(*): create Deal resource

---
+++ src/models/Deal.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+import { Schema } from 'mongoose';
+
+const dealsSchema = new Schema({
+  deal_id: {
+    type: Number,
+    required: true,
+    dropDups: true,
+    // unique: true
+  },
+  creator: {
+    type: String
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: Number,
+    required: true
+  },
+  currency: {
+    type: String,
+    required: true
+  },
+  org: {
+    type: String
+  },
+
+  status: {
+    type: String,
+    required: true,
+    default: 'active'
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.connection.model('deals', dealsSchema);
